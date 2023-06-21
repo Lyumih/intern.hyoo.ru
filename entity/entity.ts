b8f4dde9e840617e86c7e9425486916e7e69edd2
Mikhail Ivanov <32732179+Lyumih@users.noreply.github.com>
2023-06-21 11:42:16 +0300

    Update entity.ts

--- entity/entity.ts
+++ entity/entity.ts
@@ -4,56 +4,27 @@ namespace $ {
 
 		Item = null as unknown as Item
 
-		@ $mol_mem
-		fund() {
-			return this.world()!.Fund( this.Item )
-		}
-
-		@ $mol_mem
-		ids() {
+		@ $mol_mem fund() { return this.world()!.Fund( this.Item ) }
+		@ $mol_mem ids() {
 			const ids = this.list()
 			return ids
 				.map( id => $mol_int62_string_ensure( id ) )
 				.filter( $mol_guard_defined )
 		}
-
-		length() {
-			return this.ids().length
-		}
-
-		@ $mol_mem_key
-		item( id: $mol_int62_string ) {
-			return this.fund().Item( id! )
-		}
-
-		@ $mol_mem
-		items() {
-			return this.ids().map( id => this.item( id! ) )
-		}
-
-		@ $mol_action
-		item_make() {
+		length() { return this.ids().length }
+		@ $mol_mem_key item( id: $mol_int62_string ) { return this.fund().Item( id! ) }
+		@ $mol_mem items() { return this.ids().map( id => this.item( id! ) ) }
+		@ $mol_action item_make() {
 			const obj = this.fund().make()
 			this.add( obj.land.id() )
 			return obj
 		}
-		
-		@ $mol_action
-		item_push(obj: InstanceType<Item>) {
+		@ $mol_action item_push(obj: InstanceType<Item>) {
 			this.add( obj.land.id() )
 			return obj
 		}
-
-		@ $mol_action
-		item_drop( obj: InstanceType<Item>) {
-			this.drop( obj.land.id() )
-		}
-
-		@ $mol_mem_key
-		item_has( obj: InstanceType<Item> ) {
-			return this.has(obj.id())
-		}
-
+		@ $mol_action item_drop( obj: InstanceType<Item>) { this.drop( obj.land.id() ) }
+		@ $mol_mem_key item_has( obj: InstanceType<Item> ) { return this.has(obj.id()) }
 	}
 
 }
